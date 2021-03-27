3910b558343d8c4a58a9ec85d2b4a0b866760b6c
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-03-27 09:43:44 +0000

    secure session cookie

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key_goes_here"
-    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
     RATELIMIT_HEADERS_ENABLED = True
+    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key_goes_here"
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = True
