f30e2aff35540f256c77ec85185efdaca58a6f9a
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-04-14 21:52:19 +0100

    pytest formatted tests

---
+++ tests/test_error_handler.py
@@ -0,0 +1,18 @@
+from app import create_app
+
+
+def test_not_found():
+    app = create_app()
+
+    with app.test_client() as test_client:
+        response = test_client.get("/does-not-exist")
+        assert response.status_code == 404
+        assert response.mimetype == "text/html"
+
+def test_method_not_allowed():
+    app = create_app()
+
+    with app.test_client() as test_client:
+        response = test_client.post("/")
+        assert response.status_code == 405
+        assert response.mimetype == "text/html"
