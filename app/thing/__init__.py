eddbc876e898d7eec5def040df01c87ff5c251f4
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-07-07 22:33:27 +0100

    define template folder

--- app/thing/__init__.py
+++ app/thing/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint("thing", __name__)
+bp = Blueprint("thing", __name__, template_folder="../templates/thing")
 
 from app.thing import routes  # noqa: E402,F401
