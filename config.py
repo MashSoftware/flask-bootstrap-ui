0f15b45d826223a454db62cb4203574b89c8a5ed
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-08-06 16:54:39 +0100

    add point api config

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key_goes_here"
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
-    THING_API_URL = os.environ.get("THING_API_URL") or "http://localhost:3000"
+    THING_API_URL = os.environ.get("THING_API_URL") or "http://localhost:3001"
     THING_API_VERSION = os.environ.get("THING_API_VERSION") or "v1"
+    POINT_API_URL = os.environ.get("POINT_API_URL") or "http://localhost:3002"
+    POINT_API_VERSION = os.environ.get("POINT_API_VERSION") or "v1"
     TIMEOUT = os.environ.get("TIMEOUT") or 5
