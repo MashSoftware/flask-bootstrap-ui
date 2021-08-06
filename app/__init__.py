ea14cc72ef16683ed35c898e6527c94596a5f802
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-08-06 16:56:26 +0100

    support point, line and polygon blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -31,9 +31,9 @@ def create_app(config_class=Config):
         "style-src": ["https://cdn.jsdelivr.net", "'self'"],
         "script-src": ["https://cdn.jsdelivr.net", "'self'"],
         "font-src": "https://cdn.jsdelivr.net",
-        "img-src": ["data:", "'self'"],
+        "img-src": ["https://cdn.jsdelivr.net", "https://*.tile.openstreetmap.org", "data:", "'self'"],
     }
-    talisman.init_app(app, content_security_policy=csp)
+    talisman.init_app(app, content_security_policy=csp, content_security_policy_nonce_in=['script-src'])
 
     js = Bundle("src/js/*.js", filters="jsmin", output="dist/js/custom-%(version)s.js")
     css = Bundle("src/css/*.css", filters="cssmin", output="dist/css/custom-%(version)s.css")
@@ -44,12 +44,16 @@ def create_app(config_class=Config):
 
     # Register blueprints
     from app.main import bp as main_bp
+    from app.thing import bp as thing_bp
+    from app.point import bp as point_bp
+    from app.line import bp as line_bp
+    from app.polygon import bp as polygon_bp
 
     app.register_blueprint(main_bp)
-
-    from app.thing import bp as thing_bp
-
     app.register_blueprint(thing_bp, url_prefix="/things")
+    app.register_blueprint(point_bp, url_prefix="/points")
+    app.register_blueprint(line_bp, url_prefix="/lines")
+    app.register_blueprint(polygon_bp, url_prefix="/polygons")
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
