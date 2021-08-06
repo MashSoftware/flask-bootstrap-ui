742e32b9e02e2e9aab2daa17cb639dc3024afa8a
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-08-06 16:57:58 +0100

    main leaflet map setup

--- app/static/src/js/custom.js
+++ app/static/src/js/custom.js
@@ -1,1 +1,24 @@
-// Your custom JS goes here...
+// initialize Leaflet
+var map = L.map("mapid");
+
+// add the OpenStreetMap tiles
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+}).addTo(map);
+
+// add popup to each feature
+function onEachFeature(feature, layer) {
+  if (feature.properties && feature.properties.name) {
+    layer.bindPopup(feature.properties.name);
+  }
+}
+
+// add the GeoJSON feature layer
+var geojsonLayer = L.geoJSON(geojson, { onEachFeature: onEachFeature }).addTo(
+  map
+);
+
+// zoom map to GeoJSON feature layer
+map.fitBounds(geojsonLayer.getBounds());
