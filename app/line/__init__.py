58b9dc432ddfbf3225566fec170aec245d995585
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-08-31 23:19:48 +0100

    remove unused integrations & blueprints

--- app/line/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("line", __name__, template_folder="../templates/line")
-
-from app.line import routes  # noqa: E402,F401
--- app/polygon/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint("polygon", __name__, template_folder="../templates/polygon")
-
-from app.polygon import routes  # noqa: E402,F401
