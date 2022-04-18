486c4ebf2af53bdaf7771f394002934bfc816264
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-04-18 22:54:17 +0100

    sort and reduce config

--- config.py
+++ config.py
@@ -2,13 +2,11 @@ import os
 
 
 class Config(object):
+    POINT_API_URL = os.environ.get("POINT_API_URL") or "http://localhost:3002/v1"
     RATELIMIT_HEADERS_ENABLED = True
     RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key_goes_here"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
-    THING_API_URL = os.environ.get("THING_API_URL") or "http://localhost:3001"
-    THING_API_VERSION = os.environ.get("THING_API_VERSION") or "v1"
-    POINT_API_URL = os.environ.get("POINT_API_URL") or "http://localhost:3002"
-    POINT_API_VERSION = os.environ.get("POINT_API_VERSION") or "v1"
+    THING_API_URL = os.environ.get("THING_API_URL") or "http://localhost:3001/v1"
     TIMEOUT = int(os.environ.get("TIMEOUT")) or 5
