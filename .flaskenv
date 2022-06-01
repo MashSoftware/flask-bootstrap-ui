d231a0397fd9e8741367a32986cd1d0fda491e65
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-06-01 14:02:47 +0100

    move all config defaults to .env file

--- .flaskenv
+++ .flaskenv
@@ -1,5 +1,8 @@
 FLASK_APP=flask_bootstrap_ui.py
 FLASK_ENV=development
 FLASK_RUN_PORT=5000
-SECRET_KEY=o3BVSGBry#mDcbRaoDUiVF^$g!76NiEy
+POINT_API_URL=http://localhost:3002/v1
+REDIS_URL=memory://
+SECRET_KEY=c5a561b23a8fa6ab64e9ffd8bf1c7f5388377e06f0d100a4cbd511bcacdd66fc
+THING_API_URL=http://localhost:3001/v1
 TIMEOUT=5
--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config(object):
-    POINT_API_URL = os.environ.get("POINT_API_URL") or "http://localhost:3002/v1"
+    POINT_API_URL = os.environ.get("POINT_API_URL")
     RATELIMIT_HEADERS_ENABLED = True
-    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
+    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL")
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
-    THING_API_URL = os.environ.get("THING_API_URL") or "http://localhost:3001/v1"
-    TIMEOUT = int(os.environ.get("TIMEOUT")) or 5
+    THING_API_URL = os.environ.get("THING_API_URL")
+    TIMEOUT = int(os.environ.get("TIMEOUT"))
